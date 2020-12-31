6e02a9f7776fd8db9ba9b7d925488308aae06651
Kevin Ackerman <kevin.ackerman@headspring.com>
2020-12-31 08:45:14 -0600

    adds post test

---
+++ Test/Server/Tests/Data/Entities/PostTests.cs
@@ -0,0 +1,103 @@
+﻿namespace BlazorBlogWorkshop.Server.Test.Tests.Data.Entities
+{
+    using BlazorBlogWorkshop.Server.Data;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Configuration;
+    using Xunit;
+    using BlazorBlogWorkshop.Server.Data.Entities;
+    using System;
+    using System.Threading.Tasks;
+    using Shouldly;
+
+    public class PostTests
+    {
+        [Fact]
+        public async Task ShouldPersist()
+        {
+            var testProvider = TestServiceProvider
+                .WithConfiguration(config => config.AddDefaultConnectionString())
+                .WithServices((sp, config) => sp.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(config.GetConnectionString("App"))));
+
+            var createdPost = new Post
+            {
+                Id = Guid.NewGuid(),
+                Created = DateTime.UtcNow,
+                Updated = DateTime.UtcNow,
+                Slug = "slug",
+                Title = "title",
+                Body = "body"
+            };
+
+            // create post
+            await testProvider.ExecuteScopeAsync(async sp =>
+            {
+                var db = sp.GetRequiredService<AppDbContext>();
+
+                db.Posts.Add(createdPost);
+
+                await db.SaveChangesAsync();
+            });
+             
+            await testProvider.ExecuteScopeAsync(async sp =>
+            {
+                var db = sp.GetRequiredService<AppDbContext>();
+
+                var persistedPost = await db.Posts
+                    .SingleOrDefaultAsync(x => x.Id == createdPost.Id);
+
+                persistedPost.ShouldBeEquivalentTo(createdPost);
+            });
+
+            // modify post
+            Post modifiedPost = null;
+            await testProvider.ExecuteScopeAsync(async sp =>
+            {
+                var db = sp.GetRequiredService<AppDbContext>();
+
+                var persistedPost = await db.Posts
+                    .SingleOrDefaultAsync(x => x.Id == createdPost.Id);
+
+                persistedPost.Title = $"{persistedPost.Title} 2";
+
+                modifiedPost = persistedPost;
+
+                await db.SaveChangesAsync();
+            });
+
+            await testProvider.ExecuteScopeAsync(async sp =>
+            {
+                var db = sp.GetRequiredService<AppDbContext>();
+
+                var persistedPost = await db.Posts
+                    .SingleOrDefaultAsync(x => x.Id == createdPost.Id);
+
+                persistedPost.ShouldBeEquivalentTo(modifiedPost);
+            });
+
+            // delete post
+            await testProvider.ExecuteScopeAsync(async sp =>
+            {
+                var db = sp.GetRequiredService<AppDbContext>();
+
+                var persistedPost = await db.Posts
+                    .SingleOrDefaultAsync(x => x.Id == createdPost.Id);
+
+                db.Posts.Remove(persistedPost);
+
+                await db.SaveChangesAsync();
+            });
+
+            await testProvider.ExecuteScopeAsync(async sp =>
+            {
+                var db = sp.GetRequiredService<AppDbContext>();
+
+                var persistedPost = await db.Posts
+                    .SingleOrDefaultAsync(x => x.Id == createdPost.Id);
+
+                persistedPost.ShouldBeNull();
+            });
+        }
+    }
+}

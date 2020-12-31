36a5eda8b2e17cf6eb242e1f871c45551ec9cde7
Kevin Ackerman <kevin.ackerman@headspring.com>
2020-12-31 08:39:09 -0600

    adds test helper infrastructure

---
+++ Test/Server/ConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorBlogWorkshop.Server.Test
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Configuration.Memory;
+    using System.Collections.Generic;
+
+    public static class ConfigurationExtensions
+    {
+        public static void AddDefaultConnectionString(this IConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.Add(new MemoryConfigurationSource
+            {
+                InitialData = new[]
+                {
+                    KeyValuePair.Create(
+                        "ConnectionStrings:App",
+                        "Data Source=localhost;Initial Catalog=BlazorBlogWorkshop;Integrated Security=True"
+                    )
+                }
+            });
+        }
+    }
+}
---
+++ Test/Server/TestServiceProvider.cs
@@ -0,0 +1,50 @@
+﻿namespace BlazorBlogWorkshop.Server.Test
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class TestServiceProvider
+    {
+        private readonly IConfiguration _configuration;
+        private IServiceProvider _serviceProvider;
+
+        private TestServiceProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public static TestServiceProvider WithNoConfiguration() => WithConfiguration(null);
+
+        public static TestServiceProvider WithConfiguration(Action<IConfigurationBuilder> configure)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configure?.Invoke(configurationBuilder);
+
+            var configuration = configurationBuilder.Build();
+
+            return new TestServiceProvider(configuration);
+        }
+
+        public TestServiceProvider WithServices(Action<IServiceCollection> configureServices) =>
+            WithServices((sp, config) => configureServices(sp));
+
+        public TestServiceProvider WithServices(Action<IServiceCollection, IConfiguration> configureServices)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            configureServices?.Invoke(serviceCollection, _configuration);
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+
+            return this;
+        }
+
+        public void ExecuteScope(Action<IServiceProvider> execute) =>
+            execute(_serviceProvider.CreateScope().ServiceProvider);
+
+        public Task ExecuteScopeAsync(Func<IServiceProvider, Task> execute) =>
+            execute(_serviceProvider.CreateScope().ServiceProvider);
+    }
+}
